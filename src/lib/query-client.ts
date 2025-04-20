7c54b12c5f0e47a8a7809a2ef0ce95a27bda5d03
Geovane <geovanestuski2@gmail.com>
2025-04-20 15:14:10 -0300

    feat: add configuration to query client

--- src/lib/query-client.ts
+++ src/lib/query-client.ts
@@ -1,3 +1,10 @@
 import { QueryClient } from "react-query";
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: 5 * 60 * 1000,
+    },
+  },
+});
